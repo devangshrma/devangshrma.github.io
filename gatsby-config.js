473e956b12ccfdf54f2230b5d9a4765cf5f8eabf
devangshrma <sdevang19@gmail.com>
2021-02-11 15:35:12 -0800

    Replace Brittany's data with mine

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Devang Sharma',
     description:
-      'Brittany Chiang is a software engineer specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Devang Sharma is a software engineer with a history of providing software quality assurance for the products related to Telecommunications, IoT and Network Security.',
+    siteUrl: 'https://devangshrma.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
