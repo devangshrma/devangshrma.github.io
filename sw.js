d60b731596d14623480c28bbb7b971a2c7915300
devangshrma <sdevang19@gmail.com>
2021-02-13 20:12:19 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,24 +27,24 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0b6762e511bbc6e3fa5a.js"
+    "url": "webpack-runtime-4504681bcd711f3045ad.js"
   },
   {
     "url": "framework-3321703c33570853116c.js"
   },
   {
-    "url": "app-8e7e8559f7bdccaa0f8d.js"
+    "url": "app-c9e3cdd8d665d3f7aae5.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "640adc682467a54970a715f4f5996647"
+    "revision": "f48ef67484ed0b74ad3b08d60ab34293"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "25610ef15b5b4bf9e8facf44ed59a4d9"
+    "revision": "9e4fcd2225c0cd287d1a96e83b7d5f5e"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -137,7 +137,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-8e7e8559f7bdccaa0f8d.js`))) {
+  if (!resources || !(await caches.match(`/app-c9e3cdd8d665d3f7aae5.js`))) {
     return await fetch(event.request)
   }
 
